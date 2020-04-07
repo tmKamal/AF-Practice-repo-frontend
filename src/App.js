12faab7e332fda4c6c0130bed9839be21527b9f1
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-04-07 14:07:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Users from "./users/pages/users";
+import NewPlaces from "./places/pages/new-places";
+import MainNavigation from "./shared/components/navigation/main-navigation";
+import UserPlaces from "./places/pages/user-places";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/">
+        <MainNavigation></MainNavigation>
+      </Route>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users></Users>
+          </Route>
+          <Route path="/u1/new-places" exact>
+            <NewPlaces></NewPlaces>
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces></UserPlaces>
+          </Route>
+          <Redirect to="/"></Redirect>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
